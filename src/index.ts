ab407c0f21b874c35e0dbc32c8a079c1ebd85443
raphaelmonte <raphaelmontesantos@gmail.com>
2019-10-17 20:05:23 -0300

    improvements code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
+
 const app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
   res.send('hello world');
 });
